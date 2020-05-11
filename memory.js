8fca802900c84e87160f6b57a5c6ce5ba48bffc6
michal.ziolecki <michal.ziolecki@op.pl>
2020-05-11 21:19:48 +0200

    cards -> random and layout

--- memory.js
+++ memory.js
@@ -6,11 +6,9 @@ var cards = new Array();
 function randomCards(){
 	let fields  = cardsSet.length * 2;
 	while (fields > 0) {
-		console.log("round: " +  fields);
 		let random = Math.floor(cardsSet.length * Math.random());
 		let card = cardsSet[random];
 		let idx = cards.indexOf(card);
-		console.log(idx);
 		if(idx >= 0) {
 			let id = cardsSet.indexOf(card);
 			cardsSet.splice(id, 1);
@@ -18,6 +16,22 @@ function randomCards(){
 		cards.push(card);
 		fields--;
 	}
-	console.log(cards.toString());
 }
 
+function showCards(){
+	var photoDivs = "";
+	for (it = 0; it < cards.length; it++){
+		let photo = "<img class=\"photo\" src=\"img/cardBack.png\">";
+		let photoDiv = "<div class=\"photoDiv\" id=\"photoDivId" + it + "\" >" + photo +"</div>";
+		photoDivs += photoDiv;
+		if ((it + 1) %6 === 0 ) photoDivs += "<div style=\"clear: both;\"></div>";
+	}
+
+	// document.getElementById("photoContainerId").innerHTML = ;
+	$("#photoContainerId").html(photoDivs);
+}
+
+function startGame(){
+	randomCards();
+	showCards();
+}
