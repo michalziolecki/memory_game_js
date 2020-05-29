05c889123a386ef2fa8acb1ecdffdebee5ad93bc
michal.ziolecki <michal.ziolecki@op.pl>
2020-05-29 21:01:27 +0200

    compare and hide cards

--- memory.js
+++ memory.js
@@ -1,7 +1,9 @@
 var cardsSet = new Array("p1.png", "p2.png", "p3.png", "p4.png", "p5.png", "p6.png",
  "p7.png", "p8.png", "p9.png");
 var cards = new Array();
-
+var firstCard = "";
+var secondCard = "";
+var clickCounter = 0;
 
 function randomCards(){
 	let fields  = cardsSet.length * 2;
@@ -16,6 +18,7 @@ function randomCards(){
 		cards.push(card);
 		fields--;
 	}
+	console.log(cards)
 }
 
 function showCards(){
@@ -26,20 +29,33 @@ function showCards(){
 		photoDivs += photoDiv;
 		if ((it + 1) %6 === 0 ) photoDivs += "<div style=\"clear: both;\"></div>";
 	}
-
-	// document.getElementById("photoContainerId").innerHTML = ;
 	$("#photoContainerId").html(photoDivs);
 }
 
 function setClickEvents(){
 	for (let it = 0; it < cards.length; it++){
 		let id = "#photoDivId" + it;
-		console.log("test setClickEvents id: " + id);
-		console.log("test setClickEvents it: " + it);
 		$(id).on("click", function (event) {
-			showUpCard(id, it);
+			checkAndCompareCards(id, it);
 		});
 	}
+}
+
+function checkAndCompareCards(id, number){
+	console.log('CLICK: ' + clickCounter);
+	if ($(id + " img").attr("src") === "img/cardBack.png"){
+		clickCounter++;
+	}
+	if (clickCounter === 1){
+		showUpCard(id, number);
+		firstCard = id;
+	}
+	else if (clickCounter === 2) {
+		clickCounter = 0;
+		showUpCard(id, number);
+		secondCard = id;
+		compareCardsAndMakeAction();
+	} 
 }
 
 function showUpCard(id, number) {
@@ -47,6 +63,27 @@ function showUpCard(id, number) {
 	$(id).addClass("photoDivKnown");
 	$(id).removeClass("photoDivSecret");
 	$(id + " img").attr("src", img);
+}
+
+function compareCardsAndMakeAction(){
+	console.log(firstCard);
+	console.log(secondCard);
+	let first_img = $(firstCard + " img").attr("src");
+	let second_img = $(secondCard + " img").attr("src");
+	if (first_img === second_img){
+		console.log("TRUE");
+		setTimeout(hiddenAfterTimeout(firstCard, secondCard), 1000);
+	} else {
+		console.log("FALSE");
+	}
+	firstCard = "";
+	secondCard = "";
+	console.log("firstCard: " + firstCard);
+}
+
+function hiddenAfterTimeout(first_id, second_id){
+	$(first_id).fadeTo(1200, 0);
+	$(second_id).fadeTo(1200, 0);
 }
 
 function startGame(){
